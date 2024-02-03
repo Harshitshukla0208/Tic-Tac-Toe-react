5a71a70a67b1a9f96293ec205ce61844c71bec62
Harshit Shukla <mrharshit2409@gmail.com>
2024-02-03 11:40:19 +0530

    added a click sound

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,28 @@
 import Icon from '../icon/Icon';
 import './Card.css';
 
+import clickSound from '../click-sound/sound.mp3'
+
 function Card({ onPlay, player, index, gameEnd }) {
     let icon = <Icon />;
-    
+
     if (player === "X") {
         icon = <Icon name={"cross"} />;
     } else if (player === "O") {
         icon = <Icon name={"circle"} />;
     }
 
+    const handleCardClick = () => {
+        // Play the click sound only if the game is not ended and the card is empty
+        if (!gameEnd && player === "") {
+            onPlay(index);
+            const audio = new Audio(clickSound);
+            audio.play();
+        }
+    };
+
     return (
-        <div className={`card card${index + 1}`} onClick={() => !gameEnd && player == "" && onPlay(index)}>
+        <div className={`card card${index + 1}`} onClick={handleCardClick}>
             {icon}
         </div>
     );
